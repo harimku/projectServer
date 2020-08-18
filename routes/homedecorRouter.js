677e1a7bcd94a8ddacd5777719e162b2b34a0b02
Ha Rim Ku <hrku19@gmail.com>
2020-08-17 21:06:06 -0700

    Schema, Model and Router (homedecor)

--- routes/homedecorRouter.js
+++ routes/homedecorRouter.js
@@ -1,53 +1,78 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const Homedecor = require('../models/homedecor');
 
 const homedecorRouter = express.Router();
 
 homedecorRouter.use(bodyParser.json());
 
 homedecorRouter.route('/')
-.all((req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    next();
+.get((req, res, next) => {
+    Homedecor.find()   //returns promise
+    .then(homedecors => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(homedecors);
+    })
+    .catch(err => next(err));   //pass err off to the overall error handler for this express application
 })
-.get((req, res) => {
-    res.end('Will send all the homedecor products to you');
-})
-.post((req, res) => {
-    res.statusCode = 403;  //operation not supported
-    res.end('POST operation not supported on /homedecor');
+.post((req, res, next) => {
+    Homedecor.create(req.body)   //returns promise (mongoose checks if data fits schema)
+    .then(homedecor => {
+        console.log('Product Created ', homedecor);
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(homedecor);
+    })
+    .catch(err => next(err));
 })
 .put((req, res) => {
-    res.statusCode = 403;  //operation not supported
-    res.end('PUT operation not supported on /homedecor');
+    res.statusCode = 403;
+    res.end(`PUT operation not supported on /homedecors`);
 })
-.delete((req, res) => {
-    res.statusCode = 403;  //operation not supported
-    res.end('DELETE operation not supported on /homedecor');
+.delete((req, res, next) => {
+    Homedecor.deleteMany()
+    .then(response => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(response);
+    })
+    .catch(err => next(err));
 });
 
 homedecorRouter.route('/:productId')
-.all((req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    next();
-})
-.get((req, res) => {
-    res.end(`Will send details of the homedecor product: ${req.params.productId} to you`);
+.get((req, res, next) => {
+    Homedecor.findById(req.params.productId)
+    .then(homedecor => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(homedecor);
+    })
+    .catch(err => next(err));
 })
 .post((req, res) => {
     res.statusCode = 403;
-    res.end(`POST operation not supported on /homedecor/${req.params.productId}`);
+    res.end(`POST operation not supported on /homedecors/${req.params.productId}`);
 })
-.put((req, res) => {
-    res.statusCode = 403;
-    res.end(`PUT operation not supported on /homedecor/${req.params.productId}`);
+.put((req, res, next) => {
+    Homedecor.findByIdAndUpdate(req.params.productId, {
+        $set: req.body
+    }, { new: true })
+    .then(homedecor => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(homedecor);
+    })
+    .catch(err => next(err));
 })
-.delete((req, res) => {
-    res.statusCode = 403;
-    res.end(`DELETE operation not supported on /homedecor/${req.params.productId}`);
+.delete((req, res, next) => {
+    Homedecor.findByIdAndDelete(req.params.productId)
+    .then(response => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(response);
+    })
+    .catch(err => next(err));
 });
-
 
 module.exports = homedecorRouter;
