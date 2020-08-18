4d2af46a614c3a17614981f46eecf876955aa055
Ha Rim Ku <hrku19@gmail.com>
2020-08-17 18:12:04 -0700

    Install Mongoose & Connect to MongoDB server

--- app.js
+++ app.js
@@ -14,12 +14,28 @@ const outdoorsRouter = require('./routes/outdoorsRouter');
 const petRouter = require('./routes/petRouter');
 const cartRouter = require('./routes/cartRouter');
 
+//connect to the MongoDB server
+const mongoose = require('mongoose');
+const url = 'mongodb://localhost:27017/project';
+const connect = mongoose.connect(url, {
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+});
+connect.then(
+    () => console.log('Connected correctly to server'), 
+    err => console.log(err)
+);
+
+
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+// Middleware is applied to request in order of declaration
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -35,6 +51,8 @@ app.use('/fashion', fashionRouter);
 app.use('/outdoors', outdoorsRouter);
 app.use('/pet', petRouter);
 app.use('/cart', cartRouter);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
